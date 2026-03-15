52ea006c8b68f4185e98106a5a9692c4a31fedba
MyName <mymail@mail.com>
2026-03-15 23:35:42 +0700

    тест

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = "8537675451:AAGSupoPtFY_DD2ccaRkCzPwmHwKVxF5CwA"  # Получить у @BotFather
-ADMIN_IDS = [1193808132,1010770585]      # Твой ID (узнать у @userinfobot)
+ADMIN_IDS = [1193808132, 1010770585]      # Твой ID (узнать у @userinfobot)
 CHANNEL_ID = -1002041060720   # ID канала для проверки подписки
 CHANNEL_LINK = "https://t.me/+JFf2g74t-sA2ZmJi"
 DB_NAME = "database.db"
