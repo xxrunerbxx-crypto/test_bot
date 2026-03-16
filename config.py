42c5f4d642e2c022228e584609f8f3e550a458a3
MyName <mymail@mail.com>
2026-03-17 00:05:54 +0700

    правки

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "8537675451:AAGSupoPtFY_DD2ccaRkCzPwmHwKVxF5CwA"
+TOKEN = "8342601550:AAF_hU5PS4gOmuO-xA2Ro1bwjg4psHUlE0U"
 ADMIN_ID = 1193808132
 CHANNEL_ID = -1003763010069  # Теперь здесь правильный ID
 CHANNEL_LINK = "https://t.me/+JFf2g74t-sA2ZmJiл" # Здесь обычно пишут ссылку (текстом)
