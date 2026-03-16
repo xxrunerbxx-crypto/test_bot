3334b1555b3ef422017e0c1383d8e19d078109bc
MyName <mymail@mail.com>
2026-03-17 00:12:05 +0700

    правки

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = "8342601550:AAF_hU5PS4gOmuO-xA2Ro1bwjg4psHUlE0U"
-ADMIN_ID = 1193808132
+ADMIN_ID = 111385446104
 CHANNEL_ID = -1003763010069  # Теперь здесь правильный ID
 CHANNEL_LINK = "https://t.me/+JFf2g74t-sA2ZmJiл" # Здесь обычно пишут ссылку (текстом)
 DB_NAME = "database.db"
